366df21c61fe1c378d86b01f9c7ea7151e2ea0ac
mistername <36573936+mistername@users.noreply.github.com>
2019-05-17 00:20:14 +0200

    added FirstPersonView

---
+++ FirstPersonView/Camera.cs
@@ -0,0 +1,88 @@
+﻿using BepInEx;
+using RoR2;
+using System.Reflection;
+using UnityEngine;
+
+namespace Camera
+{
+    public partial class Main : BaseUnityPlugin
+    {
+        private const bool shouldShake = false;
+
+        //remade the whole function because the original function would otherwise throw an exception
+        private void CameraRigController_SetCameraState(On.RoR2.CameraRigController.orig_SetCameraState orig, CameraRigController self, CameraState cameraState)
+        {
+            if (Run.instance)
+            {
+                if (self.cameraMode == CameraRigController.CameraMode.PlayerBasic)
+                {
+                    var characterposition = self?.localUserViewer?.cachedBody?.corePosition;
+                    if (characterposition.HasValue)
+                    {
+                        cameraState.position = (Vector3)characterposition;
+                    }
+                }
+            }
+            currentstate(cameraState, self);
+            float d = (self.localUserViewer == null) ? 1f : self.localUserViewer.userProfile.screenShakeScale;
+            Vector3 position = cameraState.position;
+            if (shouldShake)
+            {
+                shake(self, cameraState);
+            }
+            Vector3 vector = self.rawScreenShakeDisplacement * d;
+            Vector3 position2 = position + vector;
+            if (vector != Vector3.zero)
+            {
+                Vector3 origin = position;
+                Vector3 direction = vector;
+                if (Physics.SphereCast(origin, self.sceneCam.nearClipPlane, direction, out RaycastHit raycastHit, vector.magnitude, LayerIndex.world.mask, QueryTriggerInteraction.Ignore))
+                {
+                    position2 = position + vector.normalized * raycastHit.distance;
+                }
+            }
+            self.transform.SetPositionAndRotation(position2, cameraState.rotation);
+            fov(self, cameraState);
+            if (self.sceneCam)
+            {
+                self.sceneCam.fieldOfView = cameraState.fov;
+            }
+        }
+
+        private static FieldInfo currentFov;
+        private static void fov(CameraRigController self, CameraState cameraState)
+        {
+            if(currentFov == null)
+            {
+                currentFov = typeof(CameraRigController).GetField("currentFov", BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+            currentFov.SetValue(self, cameraState.fov);
+        }
+        
+        private static void shake(CameraRigController self, CameraState cameraState)
+        {
+            Vector3 vector = ShakeEmitter.ComputeTotalShakeAtPoint(cameraState.position);
+            setshake(self, vector);
+        }
+
+        private static FieldInfo rawScreenShakeDisplacement;
+        private static void setshake(CameraRigController self, Vector3 vector)
+        {
+            if(rawScreenShakeDisplacement == null)
+            {
+                rawScreenShakeDisplacement = typeof(CameraRigController).GetField("rawScreenShakeDisplacement", BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+            rawScreenShakeDisplacement.SetValue(self, vector);
+        }
+
+        private static FieldInfo currentCameraState;
+        private void currentstate(CameraState cameraState, CameraRigController self)
+        {
+            if (currentCameraState == null)
+            {
+                currentCameraState = typeof(CameraRigController).GetField("currentCameraState", BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+            currentCameraState.SetValue(self, cameraState);
+        }
+    }
+}
---
+++ FirstPersonView/Main.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace Camera
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public partial class Main : BaseUnityPlugin
+    {
+        internal const string modname = "FirstPersonView";
+        internal const string version = "0.1.0";
+
+        public void Awake()
+        {
+            On.RoR2.CameraRigController.SetCameraState += CameraRigController_SetCameraState;
+        }
+
+    }
+}
