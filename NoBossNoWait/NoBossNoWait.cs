a6dc1c76a1c5999b8bf86665eee87eda6b9d4d03
mistername <36573936+mistername@users.noreply.github.com>
2019-05-22 11:35:38 +0200

    added NoBossNoWait

---
+++ NoBossNoWait/NoBossNoWait.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+
+namespace teleporter
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername.NoBossNoWait", "NoBossNoWait", "1.0.1")]
+    public class Teleporter : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.BossGroup.Awake += BossGroup_Awake;
+            On.RoR2.BossGroup.OnDestroy += BossGroup_OnDestroy;
+        }
+
+        private void BossGroup_Awake(On.RoR2.BossGroup.orig_Awake orig, RoR2.BossGroup self)
+        {
+            On.RoR2.TeleporterInteraction.FixedUpdate += TeleporterInteraction_FixedUpdate;
+            orig(self);
+        }
+
+        private void BossGroup_OnDestroy(On.RoR2.BossGroup.orig_OnDestroy orig, RoR2.BossGroup self)
+        {
+            On.RoR2.TeleporterInteraction.FixedUpdate -= TeleporterInteraction_FixedUpdate;
+            orig(self);
+        }
+
+        private void TeleporterInteraction_FixedUpdate(On.RoR2.TeleporterInteraction.orig_FixedUpdate orig, RoR2.TeleporterInteraction self)
+        {
+            if (self.isCharging)
+            {
+                    if (RoR2.BossGroup.GetTotalBossCount() == 0)
+                    {
+                        RoR2.TeleporterInteraction.instance.remainingChargeTimer = 0f;
+                    }
+            }
+            orig(self);
+        }
+    }
+}
---
+++ NoBossNoWait/README.md
@@ -0,0 +1,6 @@
+## NoBossNoWait
+
+  - Makes the teleporter charge when every boss is killed
+  
+# Installation
+Drop NoBossNoWait.dll into `\BepInEx\plugins\`
