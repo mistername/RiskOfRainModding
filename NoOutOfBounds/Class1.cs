8566daea439f4ac201a86c5ec8b379e19bcfc900
mistername <36573936+mistername@users.noreply.github.com>
2019-07-03 23:22:09 +0200

    NoOutOfBounds - Removed unnecessary code

--- NoOutOfBounds/Class1.cs
+++ NoOutOfBounds/Class1.cs
@@ -12,24 +12,6 @@ namespace NoOutOfBounds
     {
         public void Awake()
         {
-            SurvivorAPI.SurvivorCatalogReady += delegate (object s, EventArgs e)
-            {
-                
-                foreach (var survivor in RoR2.BodyCatalog.allBodyPrefabs)
-                {
-                    SurvivorDef surdef = new SurvivorDef
-                    {
-                        bodyPrefab = survivor,
-                        displayPrefab = survivor?.GetComponent<ModelLocator>()?.modelTransform?.gameObject,
-                        descriptionToken = survivor.name,
-                        primaryColor = new Color(0.87890625f, 0.662745098f, 0.3725490196f),
-                        unlockableName = "",
-                        survivorIndex = SurvivorIndex.Count
-                    };
-                    SurvivorAPI.AddSurvivor(surdef);
-                } 
-            };
-
             On.RoR2.MapZone.TeleportBody += MapZone_TeleportBody;
         }
 
