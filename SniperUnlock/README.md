f5492f927dc53a61aba92942549156e70ac05ea7
mistername <36573936+mistername@users.noreply.github.com>
2019-07-03 20:35:21 +0200

    SniperUnlock - added

---
+++ SniperUnlock/README.md
@@ -0,0 +1,15 @@
+# SniperUnlock
+	- Makes Sniper selectable
+
+## Installation
+Drop `SniperUnlock.dll` into `\BepInEx\plugins\`
+
+## Changelog
+	- v1.0.0
+		- Release
+		
+	- v1.0.1
+		- Update to Scorched Acres
+		
+	- v1.0.2
+		- Actually updated correctly
---
+++ SniperUnlock/SniperUnlock.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace HANDunlock
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class BanditMod : BaseUnityPlugin
+    {
+        internal const string modname = "SniperUnlock";
+        internal const string version = "1.0.2";
+
+        internal static ConfigFile file = new ConfigFile(Paths.ConfigPath + "\\" + modname + ".cfg", true);
+
+        public void Awake()
+        {
+            SurvivorAPI.SurvivorCatalogReady += delegate (object s, EventArgs e)
+            {
+                {
+                    var sniper = BodyCatalog.FindBodyPrefab("SniperBody");
+                    SurvivorDef item = new SurvivorDef
+                    {
+                        bodyPrefab = sniper,
+                        descriptionToken = "Sniper",
+                        displayPrefab = Resources.Load<GameObject>("Prefabs/Characters/SniperDisplay"),
+                        primaryColor = new Color(0.87890625f, 0.662745098f, 0.3725490196f),
+                        unlockableName = "",
+                        survivorIndex = SurvivorIndex.Count
+                    };
+                    SurvivorAPI.AddSurvivor(item);
+                }
+            };
+        }
+    }
+}
