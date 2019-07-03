4ead802e856209028af69eadf6ffd553521f09fa
mistername <36573936+mistername@users.noreply.github.com>
2019-07-03 20:38:59 +0200

    NoBossNoWait - rewrote to not use MMhook

--- NoBossNoWait/NoBossNoWait.cs
+++ NoBossNoWait/NoBossNoWait.cs
@@ -1,39 +1,20 @@
 ﻿using BepInEx;
+using UnityEngine;
 
 namespace teleporter
 {
     [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin("com.mistername.NoBossNoWait", "NoBossNoWait", "1.0.1")]
+    [BepInPlugin("com.mistername.NoBossNoWait", "NoBossNoWait", "1.0.2")]
     public class Teleporter : BaseUnityPlugin
     {
         public void Awake()
         {
-            On.RoR2.BossGroup.Awake += BossGroup_Awake;
-            On.RoR2.BossGroup.OnDestroy += BossGroup_OnDestroy;
+            RoR2.BossGroup.onBossGroupDefeatedServer += BossGroup_onBossGroupDefeatedServer;
         }
 
-        private void BossGroup_Awake(On.RoR2.BossGroup.orig_Awake orig, RoR2.BossGroup self)
+        private void BossGroup_onBossGroupDefeatedServer(RoR2.BossGroup obj)
         {
-            On.RoR2.TeleporterInteraction.FixedUpdate += TeleporterInteraction_FixedUpdate;
-            orig(self);
-        }
-
-        private void BossGroup_OnDestroy(On.RoR2.BossGroup.orig_OnDestroy orig, RoR2.BossGroup self)
-        {
-            On.RoR2.TeleporterInteraction.FixedUpdate -= TeleporterInteraction_FixedUpdate;
-            orig(self);
-        }
-
-        private void TeleporterInteraction_FixedUpdate(On.RoR2.TeleporterInteraction.orig_FixedUpdate orig, RoR2.TeleporterInteraction self)
-        {
-            if (self.isCharging)
-            {
-                    if (RoR2.BossGroup.GetTotalBossCount() == 0)
-                    {
-                        RoR2.TeleporterInteraction.instance.remainingChargeTimer = 0f;
-                    }
-            }
-            orig(self);
+            RoR2.TeleporterInteraction.instance.remainingChargeTimer = 0f;
         }
     }
 }
