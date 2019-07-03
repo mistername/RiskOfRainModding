6955b90208c83028c0a10c104fe98387316092cd
mistername <36573936+mistername@users.noreply.github.com>
2019-07-03 20:33:43 +0200

    Updated lib

--- lib/strip.bat
+++ lib/strip.bat
@@ -1,4 +1,7 @@
+set startTime=%time%
 pushd %~dp0
 set /p location=<mono-cil-strip_location.txt
 for /r %%v in (*.dll) do (%location% "%%v")
+echo Start Time: %startTime%
+echo Finish Time: %time%
 pause
