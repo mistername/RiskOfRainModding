eb40f504716f54c58a4cbb09ce91dbeffc246444
mistername <36573936+mistername@users.noreply.github.com>
2019-08-03 19:33:43 +0200

    using lib submodule

--- lib/strip.bat
+++
@@ -1,7 +0,0 @@
-set startTime=%time%
-pushd %~dp0
-set /p location=<mono-cil-strip_location.txt
-for /r %%v in (*.dll) do (%location% "%%v")
-echo Start Time: %startTime%
-echo Finish Time: %time%
-pause
