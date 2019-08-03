8642ee0f67d8489e71e91a5b303ab737e5c78acf
mistername <36573936+mistername@users.noreply.github.com>
2019-08-03 19:52:53 +0200

    Added start of ArmsRace

---
+++ ArmsRace/Main.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace ArmsRace
+{
+    [BepInDependency(R2API.R2API.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class ArmsRace : BaseUnityPlugin
+    {
+        //TODO wait for snackalack for model
+
+
+        internal const string modname = "ArmsRace";
+        internal const string version = "1.0.0";
+
+        public void Awake()
+        {
+            //all attackdrones attacks
+            On.EntityStates.Drone.DroneWeapon.FireGatling.OnEnter += FireGatling_OnEnter;
+            On.EntityStates.Drone.DroneWeapon.FireMegaTurret.OnEnter += FireMegaTurret_OnEnter;
+            On.EntityStates.Drone.DroneWeapon.FireMissileBarrage.OnEnter += FireMissileBarrage_OnEnter;
+            On.EntityStates.Drone.DroneWeapon.FireTwinRocket.OnEnter += FireTwinRocket_OnEnter;
+        }
+
+        //TODO after each of these item effect
+        private void FireTwinRocket_OnEnter(On.EntityStates.Drone.DroneWeapon.FireTwinRocket.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            orig(self);
+        }
+
+        private void FireMissileBarrage_OnEnter(On.EntityStates.Drone.DroneWeapon.FireMissileBarrage.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            orig(self);
+        }
+
+        private void FireMegaTurret_OnEnter(On.EntityStates.Drone.DroneWeapon.FireMegaTurret.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            orig(self);
+        }
+
+        private void FireGatling_OnEnter(On.EntityStates.Drone.DroneWeapon.FireGatling.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            orig(self);
+        }
+    }
+}
