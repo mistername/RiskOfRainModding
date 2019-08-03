988750f504579524d98c9b6d56ea08c2e337ced8
mistername <36573936+mistername@users.noreply.github.com>
2019-08-03 19:59:34 +0200

    Added BeeMovie (god why)

---
+++ BeeMovie/README.md
@@ -0,0 +1,9 @@
+# BeeMoviePlus
+Writes the bee movie script at 1 line a second during the run
+
+## Installation
+Drop `BeeMoviePlus.dll` into `\BepInEx\plugins\`
+
+## Changelog
+	- v1.0.0
+		- Release
---
+++ BeeMovie/thisisajoke.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using RoR2;
+using UnityEngine;
+using static RoR2.Chat;
+
+namespace BeeMoviePlus
+{
+    [BepInPlugin("com.mistername." + MODNAME, MODNAME, VERSION)]
+    public class BeeMoviePlus : BaseUnityPlugin
+    {
+        internal const string MODNAME = "BeeMoviePlus";
+        internal const string VERSION = "1.0.0";
+
+        public void Awake()
+        {
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+        }
+
+        private void Run_onRunStartGlobal(Run obj)
+        {
+            var coroutine = Bee();
+            StartCoroutine(coroutine);
+        }
+
+        IEnumerator Bee()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            foreach (var item in assembly.GetManifestResourceNames())
+            {
+                Debug.Log(item);
+            }
+            using (Stream stream = assembly.GetManifestResourceStream("BeeMovie.Movie.txt"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    SendBroadcastChat(new SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { line } });
+                    yield return new WaitForSeconds(1f);
+                }
+            }
+        }
+    }
+}
