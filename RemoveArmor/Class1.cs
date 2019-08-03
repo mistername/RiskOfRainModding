ae1acac2d0d2d056fc61a34ccb54f63728afdd77
mistername <36573936+mistername@users.noreply.github.com>
2019-08-03 20:07:22 +0200

    Added RemoveArmor

---
+++ RemoveArmor/Class1.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using R2API;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace RemoveArmor
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class RemoveArmor : BaseUnityPlugin
+    {
+        internal const string modname = "RemoveArmor";
+        internal const string version = "1.0.0";
+
+        public void Awake()
+        {
+            On.RoR2.BodyCatalog.Init += BodyCatalog_Init;
+            
+        }
+
+        private void BodyCatalog_Init(On.RoR2.BodyCatalog.orig_Init orig)
+        {
+            orig();
+            foreach (var item in BodyCatalog.allBodyPrefabs)
+            {
+                item.GetComponent<CharacterBody>().baseArmor = 0f;
+                item.GetComponent<CharacterBody>().levelArmor = 0f;
+            }
+            On.RoR2.BodyCatalog.Init -= BodyCatalog_Init;
+        }
+    }
+}
