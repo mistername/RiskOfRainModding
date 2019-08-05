627cf988c4f3157349598d2393afaa52d87929cc
mistername <36573936+mistername@users.noreply.github.com>
2019-08-05 22:21:19 +0200

    Added SoundPlusExample

---
+++ SoundPlusExample/README.md
@@ -0,0 +1,9 @@
+# SoundPlusExample
+	- Contains a .dll and a .sound as an example for soundplus
+	- Sound is some random guys grunt on artificer m1
+
+## Installation
+Drop `SoundPlusExample.dll` and `SoundPlusExample.sound` into `\BepInEx\plugins\`
+
+## Changelog
+	- v0.1.0
---
+++ SoundPlusExample/SoundPlusExample.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace SoundPlusExample
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInDependency("com.mistername.SoundPlus")]
+    [BepInPlugin(modguid, modname, version)]
+    public class SoundPlusExample : BaseUnityPlugin
+    {
+        public const string modguid = "com.mistername." + modname;
+        internal const string modname = "SoundPlusExample";
+        internal const string version = "0.1.0";
+
+        public void Awake()
+        {
+            On.EntityStates.Mage.Weapon.FireBolt.OnEnter += FireBolt_OnEnter;
+        }
+
+        private void FireBolt_OnEnter(On.EntityStates.Mage.Weapon.FireBolt.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            Debug.Log("posting event");
+            var id = AkSoundEngine.PostEvent(1256202815, self.outer.commonComponents.characterBody.coreTransform.gameObject);
+            Debug.Log(id.ToString());
+            orig(self);
+        }
+    }
+}
