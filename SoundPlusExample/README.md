8faee43d5cf8c66f0365071ba06c601200d796b2
mistername <36573936+mistername@users.noreply.github.com>
2019-08-06 02:01:41 +0200

    Updated the SoundPlusExample

--- SoundPlusExample/README.md
+++ SoundPlusExample/README.md
@@ -1,9 +1,12 @@
 # SoundPlusExample
-	- Contains a .dll and a .sound as an example for soundplus
+	- Contains a .dll and an embedded .sound as an example for soundplus
 	- Sound is some random guys grunt on artificer m1
 
 ## Installation
-Drop `SoundPlusExample.dll` and `SoundPlusExample.sound` into `\BepInEx\plugins\`
+Drop `SoundPlusExample.dll` and into `\BepInEx\plugins\`
 
 ## Changelog
 	- v0.1.0
+		- releae
+	- v0.2.0
+		- Now is an example of an embedded sound file
--- SoundPlusExample/SoundPlusExample.cs
+++ SoundPlusExample/SoundPlusExample.cs
@@ -1,28 +1,70 @@
 ﻿using BepInEx;
-using UnityEngine;
+using SoundPlus;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace SoundPlusExample
 {
     [BepInDependency(R2API.R2API.PluginGUID)]
-    [BepInDependency("com.mistername.SoundPlus")]
+    [BepInDependency(SoundPlus.SoundPlus.modguid)]
     [BepInPlugin(modguid, modname, version)]
     public class SoundPlusExample : BaseUnityPlugin
     {
         public const string modguid = "com.mistername." + modname;
-        internal const string modname = "SoundPlusExample";
-        internal const string version = "0.1.0";
 
-        public void Awake()
+        const string modname = "SoundPlusExample";
+        const string version = "0.2.0";
+
+        const string bankName = "SoundPlusExample.sound";
+
+        const bool embedded = true;
+
+        //You get the eventid from the wwise .txt file exported with your .bnk
+        const uint eventid = 1256202815;
+
+        internal void Awake()
         {
+            //if the bank file is embedded, if not then put a .sound in the plugin folder or any of it's subdirectories
+            if (embedded)
+            {
+                var soundbank = LoadFile(bankName);
+                if (soundbank != null)
+                {
+                    SoundBanks.Add(soundbank);
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError("soundbank fetching failed");
+                }
+            }
             On.EntityStates.Mage.Weapon.FireBolt.OnEnter += FireBolt_OnEnter;
         }
 
-        private void FireBolt_OnEnter(On.EntityStates.Mage.Weapon.FireBolt.orig_OnEnter orig, EntityStates.BaseState self)
+        //loads an embedded resource
+        internal byte[] LoadFile(string resourceName)
         {
-            Debug.Log("posting event");
-            var id = AkSoundEngine.PostEvent(1256202815, self.outer.commonComponents.characterBody.coreTransform.gameObject);
-            Debug.Log(id.ToString());
+            var assembly = Assembly.GetExecutingAssembly();
+
+            resourceName = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(resourceName));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                return reader.ReadBytes(Convert.ToInt32(stream.Length.ToString()));
+            }
+
+        }
+
+        void FireBolt_OnEnter(On.EntityStates.Mage.Weapon.FireBolt.orig_OnEnter orig, EntityStates.BaseState self)
+        {
+            //plays the eventid on the gameobject of the character when it does the firebolt attack (normally only artificer)
+            var id = AkSoundEngine.PostEvent(eventid, self.outer.commonComponents.characterBody.coreTransform.gameObject);
             orig(self);
+            //you can use the id later to stop the sound while it is still playing
+            //AkSoundEngine.StopPlayingID(id);
         }
     }
 }
