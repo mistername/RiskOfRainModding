4d817e2c9f72d798e87b3ca145043cd8ab073ea3
mistername <36573936+mistername@users.noreply.github.com>
2019-09-16 20:54:05 +0200

    Added NoItemDisplay

---
+++ NoItemDisplay/NoItemDisplay.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using System;
+
+namespace NoItemDisplay
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class NoItemDisplay : BaseUnityPlugin
+    {
+        internal const string modname = nameof(NoItemDisplay);
+        internal const string version = "1.0.0";
+
+        public void Awake()
+        {
+            On.RoR2.CharacterModel.EnableItemDisplay += this.CharacterModel_EnableItemDisplay;
+            On.RoR2.CharacterModel.SetEquipmentDisplay += CharacterModel_SetEquipmentDisplay;
+        }
+
+        private void CharacterModel_SetEquipmentDisplay(On.RoR2.CharacterModel.orig_SetEquipmentDisplay orig, RoR2.CharacterModel self, RoR2.EquipmentIndex newEquipmentIndex)
+        {
+            return;
+        }
+
+        private void CharacterModel_EnableItemDisplay(On.RoR2.CharacterModel.orig_EnableItemDisplay orig, global::RoR2.CharacterModel self, global::RoR2.ItemIndex itemIndex)
+        {
+            return;
+        }
+    }
+}
---
+++ NoItemDisplay/README.md
@@ -0,0 +1,13 @@
+# NoItemDisplay
+### Prevents Items from showing up on the config
+	- Includes Equipment
+
+## Installation
+Drop `NoItemDisplay.dll` into `\BepInEx\plugins\`
+
+## Changelog
+	- v1.0.0
+		- Release
+		
+## ToDo
+	- Config for equipment
