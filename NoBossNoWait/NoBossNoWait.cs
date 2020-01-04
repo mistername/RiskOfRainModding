60026f60daaed80722520a28d4f9707f96b6bfaa
mistername <36573936+mistername@users.noreply.github.com>
2020-01-04 18:46:11 +0100

    NoBossNoWait added hj

--- NoBossNoWait/NoBossNoWait.cs
+++ NoBossNoWait/NoBossNoWait.cs
@@ -1,19 +1,39 @@
 ﻿using BepInEx;
+using R2API.Utils;
+using RoR2;
 
 namespace teleporter
 {
-    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin("com.mistername.NoBossNoWait", "NoBossNoWait", "1.0.2")]
-    public class Teleporter : BaseUnityPlugin
+    [BepInDependency(R2API.R2API.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(Hj.HjUpdaterAPI.GUID, BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class NoBossNoWait : BaseUnityPlugin
     {
+        internal const string modname = nameof(NoBossNoWait);
+        internal const string version = "1.1.1";
         public void Awake()
         {
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(Hj.HjUpdaterAPI.GUID))
+            {
+                Updater();
+            }
             RoR2.BossGroup.onBossGroupDefeatedServer += BossGroup_onBossGroupDefeatedServer;
+        }
+
+        private static void Updater()
+        {
+            Hj.HjUpdaterAPI.Register(modname);
         }
 
         private void BossGroup_onBossGroupDefeatedServer(RoR2.BossGroup obj)
         {
-            RoR2.TeleporterInteraction.instance.remainingChargeTimer = 0f;
+            if (!TeleporterInteraction.instance) return;
+            var bossGroup = TeleporterInteraction.instance.GetFieldValue<BossGroup>("bossGroup");
+            if (!bossGroup) return;
+            if (bossGroup != obj) return;
+
+            Run.instance.fixedTime += TeleporterInteraction.instance.remainingChargeTimer;
+            TeleporterInteraction.instance.remainingChargeTimer = 0f;
         }
     }
 }
--- NoBossNoWait/README.md
+++ NoBossNoWait/README.md
@@ -1,6 +1,11 @@
 ## NoBossNoWait
 
   - Makes the teleporter charge when every boss is killed
+  - Supports [HjUpdaterAPI](https://thunderstore.io/package/Lodington/HjUpdaterAPI/) as soft dependency for auto updating 
   
+### updates
+- 1.0.0
+    release
+- 1.1.0 fixed for limbo and added support for HjUpdaterAPI
 # Installation
 Drop NoBossNoWait.dll into `\BepInEx\plugins\`
