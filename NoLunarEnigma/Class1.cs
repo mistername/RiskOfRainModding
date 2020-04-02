726ed74ec686b3b2f5ae46d6b94bfb28b593fb67
mistername <36573936+mistername@users.noreply.github.com>
2020-04-02 20:26:45 +0200

    Created NoLunarEnigma

---
+++ NoLunarEnigma/Class1.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace NoLunarEnigma
+{
+    [BepInDependency(R2API.R2API.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.mistername." + modname, modname, version)]
+    public class NoLunarEnigma : BaseUnityPlugin
+    {
+        const string modname = "NoLunarEnigma";
+        const string version = "1.0.0";
+
+        public void Awake()
+        {
+            On.RoR2.EquipmentCatalog.RegisterEquipment += EquipmentCatalog_RegisterEquipment;
+        }
+
+        private void EquipmentCatalog_RegisterEquipment(On.RoR2.EquipmentCatalog.orig_RegisterEquipment orig, EquipmentIndex equipmentIndex, EquipmentDef equipmentDef)
+        {
+            if (equipmentDef.isLunar)
+            {
+                equipmentDef.enigmaCompatible = false;
+            }
+            orig(equipmentIndex, equipmentDef);
+        }
+    }
+}
---
+++ NoLunarEnigma/README.md
@@ -0,0 +1,10 @@
+# NoLunarEnigma
+### Prevents Lunar equipment from showing up in equipment
+	- Does not prevent lunar equipment spawning
+
+## Installation
+Drop `NoLunarEnigma.dll` into `\BepInEx\plugins\`
+
+## Changelog
+	- v1.0.0
+		- Release
